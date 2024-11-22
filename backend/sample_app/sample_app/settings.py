36c184f3d6707c431afe7fe8edf1d3b6d03c5f44
Sobhan Moghimi <sobhanmoghimi45@gmail.com>
2024-11-23 01:01:09 +0330

    Fix codes

--- backend/sample_app/sample_app/settings.py
+++ backend/sample_app/sample_app/settings.py
@@ -130,9 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
